891760774db50924a22273c18c9de95f3488c23b
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-12 15:34:29 -0600

    json files all created, going to attempt the loaddata opertions now

--- gameraterapi/models/gamecategories.py
+++ gameraterapi/models/gamecategories.py
@@ -6,4 +6,4 @@ from .category import Category
 class GameCategory(models.Model):
 
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
-    content = models.CharField(max_length=333)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
